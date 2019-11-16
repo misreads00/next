e4d37e368dc024cb749cdf80349a7bc65e545b12
jiyoonhong <misreads00@gmail.com>
2019-11-16 23:23:16 +0900

    update ch6-8

--- next-learn-demo/2-using-shared-components/pages/index.js
+++ next-learn-demo/2-using-shared-components/pages/index.js
@@ -3,3 +3,15 @@ import withLayout from '../components/MyLayout';
 const Page = () => <p>Hello Next.js</p>;
 
 export default withLayout(Page);
+
+/*
+//Method 2 - Page content as a prop
+======================
+import Layout from '../components/MyLayout.js';
+
+const indexPageContent = <p>Hello Next.js</p>;
+
+export default function Index() {
+  return <Layout content={indexPageContent} />;
+}
+*/
